980064abc4b880c47e3f5a08ebdc1c8f09c3dba2
KAUR SURJIT <146156067+81960@users.noreply.github.com>
2025-04-05 20:50:10 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'  // 👈 Add this
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 👈 Fix alias for @
+    },
+  },
+  plugins: [vue()],
+})
